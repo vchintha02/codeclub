ea07df8c7e95632e22ddd5de56e134810693c4f8
venkatchaitu02 <venkatchaitu02@gmail.com>
2022-09-22 16:29:44 -0400

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 export default {
-	fetch() {
-		return new Response('Hello worker!', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
+	fetch(request) {
+		if(request.method=='POST'){
+			return new Response('Only GET requests are allowed!!!');
+			}
+		else{
+			return new Response('GET is accepted');
+			}
+
+//		return new Response('Hello worker!', {
+//			headers: {
+//				'content-type': 'text/plain',
+//			},
+//		});
 	},
 };
